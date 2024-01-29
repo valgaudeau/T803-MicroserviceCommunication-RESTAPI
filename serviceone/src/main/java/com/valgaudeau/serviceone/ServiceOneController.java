659817234fd6b4d1024f1a6ac71905ca6144a681
valgaudeau <valgaudeau@outlook.com>
2024-01-29 09:29:29 +0000

    Changed ServiceOne slightly

--- serviceone/src/main/java/com/valgaudeau/serviceone/ServiceOneController.java
+++ serviceone/src/main/java/com/valgaudeau/serviceone/ServiceOneController.java
@@ -1,17 +1,14 @@
 package com.valgaudeau.serviceone;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/service-one")
 class ServiceOneController {
 
-    @GetMapping("/call-service-two")
-    public String callServiceTwo() {
+    @GetMapping("/call-service-one")
+    public String callServiceOne() {
         return "hi from service one";
     }
 }
